3a215098c8b9bdbae6644dd5cc26fb9feff7e09c
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-01-26 15:54:35 -0800

    badabodobedo

--- src/lib.rs
+++ src/lib.rs
@@ -158,3 +158,30 @@ pub fn add(left: usize, right: usize) -> usize {
 //         Guess::new(200);
 //     }
 // }
+
+// $ cargo test -- --test-threads=1 <---- changes how tests are run, able to just run one at a ytime instead of all at once?
+// $ cargo test -- --show-output
+
+// Shwoing Function Output
+
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got hte value {}", a);
+    10
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
+    }
+}
